8530647e8a7f95d7321b28301c31a46c2c86db04
kylie-kiaying <kylie.kiayingchua@gmail.com>
2024-01-20 01:22:37 +0800

    update navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import Link from 'next/link';
+// /components/Navbar.tsx
 
-const NavBar = () => {
+import React from 'react';
+import Image from 'next/image';
+
+const Navbar = () => {
   return (
-    <nav className="bg-black text-white shadow-md fixed w-full z-10">
-      <div className="container mx-auto px-6 py-3 flex justify-between items-center">
-        <div className="flex items-center">
-          <div className="text-lg font-semibold">
-            <Link href="/">
-              Kylie Chua
-            </Link>
-          </div>
-        </div>
-        <div className="flex items-center space-x-4">
-          <Link href="/about">ABOUT</Link>
-          <Link href="/experience">EXPERIENCE</Link>
-          <Link href="/projects">PROJECTS</Link>
-          <Link href="/contact">CONTACT</Link>
-        </div>
-      </div>
+    <nav className="bg-black text-white flex justify-between items-center p-4">
+      <div className="font-bold text-lg">Kylie Chua</div>
     </nav>
   );
-}
+};
 
-export default NavBar;
+export default Navbar;
