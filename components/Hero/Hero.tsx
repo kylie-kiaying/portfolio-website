8fac800ba9c03e23448ca080762fd73b3fbba291
kylie-kiaying <kylie.kiayingchua@gmail.com>
2024-01-25 01:51:46 +0800

    modified description

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React from "react";
 import NameAnimation from "@/components/Hero/NameAnimations";
 
 const Hero = () => {
   return (
-    <div id="overview" className="flex flex-col text-center md:flex-row items-start md:items-center">
+    <div
+      id="overview"
+      className="flex flex-col text-center md:flex-row items-start md:items-center"
+    >
       <div className="flex-1">
         <h2 className="text-8xl mt-40 font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-800 to-yellow-400">
           Hi, I am
         </h2>
         <NameAnimation />
-        <p className="text-gray-200 text-xl">
-          I am a first year Computer Science student at Singapore Management University hoping to pursue a career in Software Engineering.
-          <br/>
-          Feel free to drop me a message and connect!
+        <p className="text-center text-lg md:text-xl text-gray-400 max-w-4xl mx-auto mb-4">
+          Welcome to my website!
+        </p>
+        <p className="text-center text-lg md:text-xl text-gray-400 max-w-4xl mx-auto mb-4">
+          I am a first-year Computer Science student at Singapore Management
+          University, embarking on an exciting journey in the world of
+          technology.
+        </p>
+        <p className="text-center text-lg md:text-xl text-gray-400 max-w-4xl mx-auto mb-12">
+          As an aspiring software engineer with a deep passion for developing
+          innovative products, I am committed to continuous learning and skill
+          enhancement. I am always on the lookout for opportunities to learn and
+          connect with like-minded individuals.
         </p>
       </div>
-
     </div>
   );
-}
+};
 
 export default Hero;
--- components/Socials/Socials.tsx
+++ components/Socials/Socials.tsx
@@ -4,6 +4,11 @@ const ContactMe: React.FC = () => {
   return (
     <div id="socials" className="p-8">
       <h2 className="abosulte m-16 uppercase tracking-[20px] text-gray-500 font-bold text-center text-6xl">Socials</h2>
+      <p className="text-center text-lg md:text-xl text-gray-400 max-w-2xl mx-auto mb-12">
+        Feel free to check out any of my socials.
+        <br/>
+        Do drop me a message and am happy to chat anytime!
+      </p>
       <div className="flex flex-col items-center justify-center space-y-4">
         <a
           href="mailto:kylie.kiayingchua@gmail.com"
